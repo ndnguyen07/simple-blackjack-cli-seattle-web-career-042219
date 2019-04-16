bf8317cba209e3d3893d093fa96994130aea6c14
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-16 09:02:16 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -29,7 +29,18 @@ def initial_round
 end
 
 def hit?
-  # code hit? here
+  prompt_user
+  input = get_user_input
+  until input == 'h' || input == 's'
+    invalid_command
+    prompt_user
+    input = get_user_input
+  end
+  if input == 'h'
+    card_total += deal_card
+  elsif input == 's'
+    card_total
+  end
 end
 
 def invalid_command
